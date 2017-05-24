d9052123798b889d9c6728069e655d542a09595c
brunoediaz <brunoediaz18@gmail.com>
2017-05-23 23:02:04 -0300

    Proyecto de prueba para Servicio web API creado

---
+++ Código/ProyectoFinal/WebAPI/Controllers/carroController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class carroController : ApiController
+    {
+        Carro[] carros = new Carro[]
+        {
+            new Carro { idCarro = 1, marca = "Ferrari", modelo=2012 },
+            new Carro { idCarro = 2, marca = "BMW", modelo=2010 },
+            new Carro { idCarro = 3, marca = "Mazda", modelo=2002 },
+            new Carro { idCarro = 4, marca = "Nissan", modelo=2004 },
+            new Carro { idCarro = 5, marca = "Renault", modelo=1998 }
+        };
+
+        public IEnumerable<Carro> GetAllCarro()
+        {
+            return carros;
+        }
+
+        public IHttpActionResult GetCarro(int id)
+        {
+            var carro = carros.FirstOrDefault((c) => c.idCarro == id);
+
+            if(carro != null)
+            {
+                return Ok(carro);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Código/ProyectoFinal/WebAPI/Models/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Carro
+    {
+        public int idCarro { get; set; }
+        public string marca { get; set; }
+        public int modelo { get; set; }
+    }
+}
