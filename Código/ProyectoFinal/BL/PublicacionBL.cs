84888f491a64d92faaa60c2dfa7237c4587a954f
LAPTOP-G36SIJ74\Usuario <fsperonip@hotmail.com>
2017-06-24 10:49:29 -0300

    Diagrama de clases, Mer, Base de datos y estructira Back-end. Falta construir clase comentariosPuntuacion en capa ET. Capa DAL de Publicacion incompleta.

---
+++ Código/ProyectoFinal/BL/PublicacionBL.cs
@@ -0,0 +1,58 @@
+﻿using DAL;
+using ET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class PublicacionBL
+    {
+        private PublicacionDAL publicacionDAL = new PublicacionDAL();
+
+        public void altaPublicacion(Publicacion publicacion)
+        {
+            validarPublicacion(publicacion);
+            publicacionDAL.altaPublicacion(publicacion);
+        }
+        public void actualizarPublicacion(Publicacion publicacion)
+        {
+            validarPublicacion(publicacion);
+            publicacionDAL.actualizarPublicacion(publicacion);
+        }
+        public List<Publicacion> obtenerTodos()
+        {
+            return publicacionDAL.obtenerTodos();
+        }
+        public Publicacion obtener(int id)
+        {
+            return publicacionDAL.obtener(id);
+        }
+        public List<Publicacion> obtenerTodosCliente(int idCliente)
+        {
+            return publicacionDAL.obtenerTodosCliente(idCliente);
+        }
+        public void habilitarPublicacion(int id)
+        {
+            publicacionDAL.habilitarPublicacion(id);
+        }
+        public void deshabilitarPublicacion(int id)
+        {
+            publicacionDAL.deshabilitarPublicacion(id);
+        }
+        //VER VALIDACIONES
+        private void validarPublicacion(Publicacion publicacion)
+        {
+            //if (publicacion.Respuestas == null)
+            //{
+            //    throw new ProyectoException("Error: La publicación debe contener respuestas asociadas");
+            //}
+            if (publicacion.Titulo.Length < 3)
+            {
+                throw new ProyectoException("Error: Título debe contener al menos 3 caracteres");
+            }
+        }
+    }
+}
---
+++ Código/ProyectoFinal/DAL/PublicacionDAL.cs
@@ -0,0 +1,41 @@
+﻿using ET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PublicacionDAL
+    {
+        public void altaPublicacion(Publicacion publicacion)
+        {
+
+        }
+        public void actualizarPublicacion(Publicacion publicacion)
+        {
+            
+        }
+        public List<Publicacion> obtenerTodos()
+        {
+            return null;
+        }
+        public List<Publicacion> obtenerTodosCliente(int idCliente)
+        {
+            return null;
+        }
+        public Publicacion obtener(int id)
+        {
+            return null;
+        }
+        public void habilitarPublicacion(int id)
+        {
+            
+        }
+        public void deshabilitarPublicacion(int id)
+        {
+            
+        }
+    }
+}
