700517fd3535afc3fe5a7c200feae840bba3a59c
LAPTOP-G36SIJ74\Usuario <fsperonip@hotmail.com>
2017-07-23 13:09:06 -0300

    inicio back comentarios y puntuacion...

---
+++ Código/ProyectoFinal/BL/ComentarioPuntuacionBL.cs
@@ -0,0 +1,51 @@
+﻿using DAL;
+using ET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ComentarioPuntuacionBL
+    {
+        private ComentarioPuntuacionDAL comentarioPuntuacionDAL = new ComentarioPuntuacionDAL();
+
+        //VER SI ES NECESARIO ELIMINAR UNA PREGUNTA
+        public void altaComentarioPuntuacion(ComentarioPuntuacion comentarioPuntuacion)
+        {
+            validarComentarioPuntuacion(comentarioPuntuacion);
+            comentarioPuntuacionDAL.altaComentarioPuntuacion(comentarioPuntuacion);
+        }
+        //Solo para administradores
+        public void borrarComentarioPuntuacion(ComentarioPuntuacion comentarioPuntuacion)
+        {         
+            comentarioPuntuacionDAL.borrarComentarioPuntuacion(comentarioPuntuacion);
+        }
+        public List<ComentarioPuntuacion> obtenerPorPublicacion(int idPublicacion)
+        {
+            return comentarioPuntuacionDAL.obtenerPorPublicacion(idPublicacion);
+        }
+        //ver validaciones
+        private void validarComentarioPuntuacion(ComentarioPuntuacion comentarioPuntuacion)
+        {
+            if (comentarioPuntuacion.Comentario.Length < 2)
+            {
+                throw new ProyectoException("Error: Comentario");
+            }
+            if (comentarioPuntuacion.Puntuacion < 0 || comentarioPuntuacion.Puntuacion >5)
+            {
+                throw new ProyectoException("Error: Puntuación");
+            }
+            if (comentarioPuntuacion.Publicacion.Id == 0)
+            {
+                throw new ProyectoException("Error: Id Publicación");
+            }
+            if (comentarioPuntuacion.Cliente.Id == 0)
+            {
+                throw new ProyectoException("Error: Id Cliente");
+            }
+        }
+    }
+}
---
+++ Código/ProyectoFinal/DAL/ComentarioPuntuacionDAL.cs
@@ -0,0 +1,65 @@
+﻿using ET;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ComentarioPuntuacionDAL
+    {
+        public void altaComentarioPuntuacion(ComentarioPuntuacion comentarioPuntuacion)
+        {
+            string cadenaInsertPregunta = @"INSERT INTO COMENTARIOPUNTUACION VALUES(@comentario, @puntuacion,@idPublicacion,@idCliente);";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Utilidades.conn))
+                {
+                    using (SqlCommand cmd = new SqlCommand(cadenaInsertPregunta, con))
+                    {
+                        cmd.Parameters.AddWithValue("@comentario", comentarioPuntuacion.Comentario);
+                        cmd.Parameters.AddWithValue("@puntuacion", comentarioPuntuacion.Puntuacion);
+                        cmd.Parameters.AddWithValue("@idPublicacion", comentarioPuntuacion.Publicacion.Id);
+                        cmd.Parameters.AddWithValue("@idCliente", comentarioPuntuacion.Cliente.Id);
+
+                        con.Open();
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ProyectoException("Error: " + ex.Message);
+            }
+        }
+        public void borrarComentarioPuntuacion(ComentarioPuntuacion comentarioPuntuacion)
+        {
+            string cadenaInsertPregunta = @"DELETE FROM COMENTARIOPUNTUACION WHERE @idComentario;";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Utilidades.conn))
+                {
+                    using (SqlCommand cmd = new SqlCommand(cadenaInsertPregunta, con))
+                    {
+                        cmd.Parameters.AddWithValue("@idComentario", comentarioPuntuacion.Id);
+
+                        con.Open();
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ProyectoException("Error: " + ex.Message);
+            }
+        }
+        public List<ComentarioPuntuacion> obtenerPorPublicacion(int idPublicacion)
+        {
+            return null;
+        }
+    }
+}
